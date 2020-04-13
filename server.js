4c892412f752f91c65da0ba8db2a8ee02845106d
Mateusz Brzozowski <neertoon@icloud.com>
2020-04-13 20:58:40 +0200

    Socket - broadcast informacji dla innych użytkowników i komunikat przy odłączeniu

--- server.js
+++ server.js
@@ -12,9 +12,15 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Run when client connect 
 io.on('connection', socket => {
-    console.log('New web socket connection...');
-    
     socket.emit('message', 'Welcome to The Game');
+    
+    // Broadcast when user connect
+    socket.broadcast.emit('message', 'The user joined The Game');
+    
+    // Gdy użytkownik się odłącza - uwaga, może go wywalić z gry a będzie chciał powrócić!
+    socket.on('disconnect', () => {
+        io.emit('message', 'User has left The Game (but we dont know if he will return)'); 
+    });
 });
 
 const PORT = 3000 || process.env.PORT;
