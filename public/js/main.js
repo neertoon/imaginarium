ea3c4ca2f746aa4ec392cada06bdd5c9cdd8de47
Mateusz Brzozowski <neertoon@icloud.com>
2020-04-13 21:07:57 +0200

    Socket - wysyłanie wiadomośći od klienta do serwera i odsyłanie ich z powrotem do innych klientów (chat function)

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,16 @@
+const chatForm = document.getElementById('chat-form');
+
 const socket = io();
 
 socket.on('message', message => {
     console.log(message);
+});
+
+chatForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    
+    const msg = e.target.elements.msg.value;
+
+    // Emit message to server
+    socket.emit('chatMessage', msg);
 });
--- server.js
+++ server.js
@@ -21,6 +21,11 @@ io.on('connection', socket => {
     socket.on('disconnect', () => {
         io.emit('message', 'User has left The Game (but we dont know if he will return)'); 
     });
+    
+    // Listen for chatMessages
+    socket.on('chatMessage', (msg) => {
+        io.emit('message', msg);
+    });
 });
 
 const PORT = 3000 || process.env.PORT;
